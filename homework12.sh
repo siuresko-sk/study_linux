dd170367bd762f6750275fef1c9d41123ac8041a
siuresko-sk <s.iuresko@gmail.com>
2026-06-24 17:30:05 +1000

    Add task 4

--- homework12.sh
+++ homework12.sh
@@ -33,3 +33,34 @@ esac
 echo $shb > "$1"
 touch "$1" 
 chmod u+x "$1"
+
+3) modfilter
+
+"#!/usr/bin/env bash"
+flag=""
+case "$1" in
+  r | read)
+    flag="-readable";;
+  w | write)
+    flag="-writable";;
+  x | execute)
+    flag="-executable";;
+  *)
+    echo "Флаг не определен"
+    exit 1
+esac
+find . $flag -ls
+
+4) shebanger
+#!/usr/bin/env bash
+dest_dir=""
+if [[ "$1" == "-e" || "$1" == "--export" ]] ; then
+  dest_dir="$2"
+  find . -type f \( -name "*.sh" -or -name "*.bash" \) | while read -r file; do
+    chmod u+x $file
+    name=$(basename "$file")
+    mv "$file" "$dest_dir/${name%.*}"
+  done
+else 
+  find . -type f \( -name "*.sh" -or -name "*.bash" \) -exec chmod u+x {} +
+fi
