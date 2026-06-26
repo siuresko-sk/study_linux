73612ae5777f661aeb1156087f4d21a8bd920c98
siuresko-sk <s.iuresko@gmail.com>
2026-06-26 12:36:09 +1000

    Update homework14.sh

--- homework14.sh
+++ homework14.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 while true ; do
   usage=$(df --output=pcent $1 | tail -n +2 | sed "s/\%//" ) 
-  if [[ $usage -gt 90 ]] ; then
+  if (( $usage > 90 )) ; then
     echo "Внимание! Дисковое пространство заполнено более чем на 90%"
   fi
   sleep 5m
@@ -12,7 +12,7 @@ chmod u+x dfalarm.sh
 
 2) wgetalarm.sh
 #!/usr/bin/env bash
-if wget -qO - https://true-time.com/moscow/ | grep -Eq "00" ; then
+if wget -qO - https://true-time.com/moscow/ | grep 'class="hour-minutes-string"' | grep -q '00' ; then
   echo "Alarm!"
 fi
 
@@ -24,6 +24,6 @@ bash spamer --instances 5 --delay 1 ./wgetalarm.sh
 curl -s "https://jsonplaceholder.typicode.com/posts/99" 
 for (( i=1; i<4; i++ )) ; do
   curl -X POST "https://jsonplaceholder.typicode.com/posts/" \
-    -H "Content-Type: application/json" \
+    -H "Content-Type: application/json; charset=UTF-8" \
     -d "{\"title\": \"post $i\", \"body\": \"content\", \"userId\": 1}"
 done 
